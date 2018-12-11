bef9668850cbc603634563c1631ad14506c59b80
ChoiDeborah <adorablejimini@gmail.com>
2018-12-11 17:27:29 +0900

    add Example02-4.py

---
+++ Example02-4.py
@@ -0,0 +1,23 @@
+# Python Tuple
+
+# Example 1
+
+t1 = (3)
+# t1 = 3
+
+print(t1)
+
+# Example 2
+
+t2 = (1, 2, 3)
+
+# t2[1] = 4
+# Tuple is immutable Object
+
+# Example_3
+
+t3 = (1, 2, 3)
+
+t3 = t3 + (4,)
+
+print(t3)
